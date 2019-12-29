e53986d17386e1ad99eb8b3a22b35c09178b996a
John Warmann <john.warmann@gmail.com>
2019-12-29 13:15:56 -0500

    Initial Commit

---
+++ LocalizeWiz/LocalizeWiz.h
@@ -0,0 +1,19 @@
+//
+//  LocalizeWiz.h
+//  LocalizeWiz
+//
+//  Created by John Warmann on 2019-12-29.
+//  Copyright © 2019 LocalizeWiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalizeWiz.
+FOUNDATION_EXPORT double LocalizeWizVersionNumber;
+
+//! Project version string for LocalizeWiz.
+FOUNDATION_EXPORT const unsigned char LocalizeWizVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalizeWiz/PublicHeader.h>
+
+
